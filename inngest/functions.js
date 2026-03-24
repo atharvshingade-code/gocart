4d8b62099494c649d5375b2e490e18bfca3b37df
atharvshingade-code <atharvshingade7@gmail.com>
2026-03-24 21:34:46 +0530

    funditon fix

--- inngest/functions.js
+++ inngest/functions.js
@@ -1,59 +1,59 @@
 import { inngest } from "./client";
 
-export const someFunction = async () => {
-  const { prisma } = await import("@/lib/prisma");
-
-  const users = await prisma.user.findMany();
-  return users;
-};
-
 export const syncUserCreation = inngest.createFunction(
-  { id: "sync-user-creation" },
-  { event: "clerk/user.created" },
+  {
+    id: "sync-user-creation",
+    triggers: { event: "clerk/user.created" },
+  },
   async ({ event }) => {
-    const {data} = event
+    const { prisma } = await import("@/lib/prisma");
+
+    const { data } = event;
+
     await prisma.user.create({
       data: {
         id: data.id,
         email: data.email_addresses[0].email_address,
         name: `${data.first_name} ${data.last_name}`,
         image: data.image_url,
-        
-        
-      }
-    })
-
+      },
+    });
   }
-)
+);
 
 export const syncUserUpdation = inngest.createFunction(
-  { id: "sync-user-updation" },
-  { event: "clerk/user.updated" },
+  {
+    id: "sync-user-updation",
+    triggers: { event: "clerk/user.updated" },
+  },
   async ({ event }) => {
-    const {data} = event
+    const { prisma } = await import("@/lib/prisma");
+
+    const { data } = event;
+
     await prisma.user.update({
-      where: {
-        id: data.id
-      },
+      where: { id: data.id },
       data: {
         email: data.email_addresses[0].email_address,
         name: `${data.first_name} ${data.last_name}`,
-        image: data.image_url
-      }
-    })
+        image: data.image_url,
+      },
+    });
   }
-)
-
+);
 
 export const syncUserDeletion = inngest.createFunction(
-  { id: "sync-user-delete" },
-  { event: "clerk/user.deleted" },
+  {
+    id: "sync-user-delete",
+    triggers: { event: "clerk/user.deleted" },
+  },
   async ({ event }) => {
-    const {data} = event
+    const { prisma } = await import("@/lib/prisma");
+
+    const { data } = event;
+
     await prisma.user.delete({
-      where: {
-        id: data.id
-      }
-    })
+      where: { id: data.id },
+    });
   }
-)
+);
